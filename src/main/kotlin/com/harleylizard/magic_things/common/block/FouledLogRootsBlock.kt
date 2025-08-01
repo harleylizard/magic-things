b77de5c2a926b838100d03270961bda1003eb36d
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-08-01 22:38:46 +0930

    account for top

--- src/main/kotlin/com/harleylizard/magic_things/common/block/FouledLogRootsBlock.kt
+++ src/main/kotlin/com/harleylizard/magic_things/common/block/FouledLogRootsBlock.kt
@@ -1,7 +1,13 @@
 package com.harleylizard.magic_things.common.block
 
+import com.harleylizard.magic_things.common.MagicThingsBlocks
+import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.item.context.BlockPlaceContext
+import net.minecraft.world.level.LevelAccessor
+import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
@@ -15,5 +21,34 @@ class FouledLogRootsBlock(properties: Properties) : Block(properties) {
     override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block?, BlockState?>) {
         builder.add(BlockStateProperties.HORIZONTAL_FACING)
     }
+
+    override fun updateShape(blockState: BlockState, direction: Direction, blockState2: BlockState, levelAccessor: LevelAccessor, blockPos: BlockPos, blockPos2: BlockPos): BlockState? {
+        return blockState.takeIf { canSurvive(blockState, levelAccessor, blockPos) } ?: Blocks.AIR.defaultBlockState()
+    }
+
+    override fun canSurvive(blockState: BlockState, levelReader: LevelReader, blockPos: BlockPos): Boolean {
+        for (direction in Direction.Plane.HORIZONTAL) {
+            if (nextToLog(levelReader, blockPos, direction)) {
+                return isTopSolid(levelReader, blockPos.below())
+            }
+        }
+
+        return false
+    }
+
+    override fun getStateForPlacement(context: BlockPlaceContext): BlockState {
+        return defaultBlockState().let {
+            for (direction in Direction.Plane.HORIZONTAL) {
+                if (nextToLog(context.level, context.clickedPos, direction)) {
+                    return@let it.setValue(BlockStateProperties.HORIZONTAL_FACING, direction)
+                }
+            }
+            it
+        }
+    }
+
+    fun isTopSolid(level: LevelReader, blockPos: BlockPos) = level.getBlockState(blockPos).let { it.isFaceSturdy(level, blockPos, Direction.UP) && !it.propagatesSkylightDown(level, blockPos) }
+
+    fun nextToLog(level: LevelReader, blockPos: BlockPos, direction: Direction) = level.getBlockState(blockPos.relative(direction)).`is`(MagicThingsBlocks.FOULED_LOG)
 
 }
--- src/main/kotlin/com/harleylizard/magic_things/common/terrablender/FouledTreeFeature.kt
+++ src/main/kotlin/com/harleylizard/magic_things/common/terrablender/FouledTreeFeature.kt
@@ -4,8 +4,8 @@ import com.harleylizard.magic_things.common.MagicThingsBlocks
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
 import net.minecraft.tags.BlockTags
-import net.minecraft.world.level.WorldGenLevel
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
 import net.minecraft.world.level.levelgen.feature.Feature
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext
@@ -15,47 +15,50 @@ class FouledTreeFeature() : Feature<NoneFeatureConfiguration>(NoneFeatureConfigu
 
     override fun place(context: FeaturePlaceContext<NoneFeatureConfiguration>): Boolean {
         val level = context.level()
+        val random = context.random()
         val blockPos = context.origin()
 
-        val random = context.random()
-
-        val log = MagicThingsBlocks.FOULED_LOG.defaultBlockState()
         val height = 4 + random.nextInt(3)
-        for (i in 0..height) {
-            level.setBlock(blockPos.above(i), log, Block.UPDATE_ALL)
-        }
 
-        var lowest = 0
+        val top = blockPos.above(height)
+
+        var floor = top.y
         for (direction in Direction.Plane.HORIZONTAL) {
             if (random.nextInt(8) == 0) {
                 continue
             }
-
-            val offset = blockPos.relative(direction)
+            var relative = top.relative(direction)
 
             var i = 0
-            var below = offset
-            while (!level.getBlockState(offset.below(i).also { below = it }).isFaceSturdy(level, below, Direction.UP) && i < 8) {
-                i++
+            while (level.getBlockState(relative.below().also { relative = it }).`is`(BlockTags.REPLACEABLE)) {
+                if (i++ > 8) {
+                    break
+                }
             }
-            val rootBlockPos = below.above()
 
-            if (canReplace(level, rootBlockPos)) {
+            val lowest = relative.above()
+
+            if (level.getBlockState(lowest).let { it.fluidState.isEmpty && replaceable(it) }) {
                 val root = MagicThingsBlocks.FOULED_LOG_ROOTS.defaultBlockState().setValue(BlockStateProperties.HORIZONTAL_FACING, direction.opposite)
 
-                if (root.canSurvive(level, rootBlockPos) && level.setBlock(below.above(), root, Block.UPDATE_ALL)) {
-                    lowest = lowest.coerceAtLeast(i)
-                }
+                level.setBlock(lowest, root, Block.UPDATE_ALL)
+
+                floor = Math.min(floor, lowest.y)
             }
         }
 
-        for (i in lowest - 1 downTo 0) {
-            level.setBlock(blockPos.below(i), log, Block.UPDATE_ALL)
+        for (i in floor..top.y) {
+            val relative =  BlockPos(blockPos.x, i, blockPos.z)
+
+            level.getBlockState(relative).takeIf { replaceable(it) || it.`is`(BlockTags.DIRT) }?.let {
+                level.setBlock(relative, MagicThingsBlocks.FOULED_LOG.defaultBlockState(), Block.UPDATE_ALL)
+            }
+
         }
 
         return true
     }
 
-    fun canReplace(level: WorldGenLevel, blockPos: BlockPos) = level.getBlockState(blockPos).let { it.fluidState.isEmpty && (it.`is`(BlockTags.REPLACEABLE_BY_TREES) || it.`is`(BlockTags.REPLACEABLE)) }
+    fun replaceable(blockState: BlockState) = blockState.`is`(BlockTags.REPLACEABLE_BY_TREES) || blockState.`is`(BlockTags.REPLACEABLE)
 
 }
