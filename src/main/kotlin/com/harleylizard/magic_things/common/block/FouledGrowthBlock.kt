f478484848dbc99a04f9dcfec3e66cdf9e21372d
harleylizard <64570529+harleylizard@users.noreply.github.com>
2025-08-02 02:38:31 +0930

    wip packing

--- src/main/kotlin/com/harleylizard/magic_things/common/block/FouledGrowthBlock.kt
+++ src/main/kotlin/com/harleylizard/magic_things/common/block/FouledGrowthBlock.kt
@@ -2,8 +2,10 @@ package com.harleylizard.magic_things.common.block
 
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.level.block.state.StateDefinition
 import net.minecraft.world.level.block.state.properties.BlockStateProperties
@@ -22,7 +24,40 @@ class FouledGrowthBlock(properties: Properties) : Block(properties) {
     }
 
     override fun updateShape(blockState: BlockState, direction: Direction, blockState2: BlockState, levelAccessor: LevelAccessor, blockPos: BlockPos, blockPos2: BlockPos): BlockState? {
-        return blockState
+        val i = fromBlockState(blockState)
+
+        for (direction in Direction.Plane.HORIZONTAL) {
+        }
+
+        return if (i == 0) Blocks.AIR.defaultBlockState() else reverse(i)
+    }
+
+    override fun getStateForPlacement(blockPlaceContext: BlockPlaceContext): BlockState {
+        return defaultBlockState().setValue(sides, (3 shl (2 * 0)) or (1 shl (2 * 1)))
+    }
+
+    fun fromBlockState(blockState: BlockState): Int {
+        var i = 0
+        i = i or ((if (blockState.getValue(BlockStateProperties.UP)) 1 else 0) shl 0)
+        i = i or ((if (blockState.getValue(BlockStateProperties.DOWN)) 1 else 0) shl 1)
+
+        val j = blockState.getValue(sides)
+
+        for (direction in Direction.Plane.HORIZONTAL) {
+            val ordinal = direction.ordinal
+
+            val value = (j shr (2 * (ordinal - 2))) and 3
+
+            i = i or ((if (value > 0) 1 else 0) shl ordinal)
+        }
+
+        return i
+    }
+
+    fun reverse(i: Int): BlockState {
+        var result = defaultBlockState()
+
+        return result
     }
 
     companion object {
